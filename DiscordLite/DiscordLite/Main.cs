1bced55fbc1eedfccf28c222f38dc175c73c8707
Trent Patterson <jtrent238@outlook.com>
2019-02-12 20:04:22 -0500

    2122019804

---
+++ DiscordLite/DiscordLite/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscordLite
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://discordapp.com/channels/@me");
+        }
+    }
+}
